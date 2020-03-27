074cb69121758c7968477cb9ff8dfef56451898e
JC Gibson <joseph.gibson@punchkick.com>
2020-03-27 17:05:30 -0400

    Capture Calling Stack during Thread spawn for Papyrus Executor.

--- util/src/main/java/papyrus/util/PapyrusExecutor.kt
+++ util/src/main/java/papyrus/util/PapyrusExecutor.kt
@@ -3,13 +3,14 @@ package papyrus.util
 import android.os.Handler
 import android.os.Looper
 import android.os.Process
+import android.util.Log
 import java.util.*
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
 object PapyrusExecutor {
     val uiHandler = Handler(Looper.getMainLooper())
-    val ui = Executor { command -> uiHandler.post(command) }
+    val ui = Executor { command -> uiHandler.post(StackTraceRunnable { command.run() }) }
     val background: Executor = Executors.newCachedThreadPool { r ->
         Thread(Runnable {
             Process.setThreadPriority(Process.THREAD_PRIORITY_DISPLAY)
@@ -19,29 +20,50 @@ object PapyrusExecutor {
 
     @Synchronized
     fun ui(runnable: () -> Unit) {
-        ui.execute(runnable)
+        ui.execute(StackTraceRunnable { runnable() })
     }
 
     @Synchronized
     fun background(runnable: () -> Unit) {
-        background.execute(runnable)
+        background.execute(StackTraceRunnable { runnable() })
     }
 
     @Synchronized
     fun ui(timeout: Long, runnable: () -> Unit) {
+        val wrapped = StackTraceRunnable {
+            runnable()
+        }
         Timer().schedule(object : TimerTask() {
             override fun run() {
-                ui.execute(runnable)
+                ui.execute(wrapped)
             }
         }, timeout)
     }
 
     @Synchronized
     fun background(timeout: Long, runnable: () -> Unit) {
+        val wrapped = StackTraceRunnable {
+            runnable()
+        }
         Timer().schedule(object : TimerTask() {
             override fun run() {
-                background.execute(runnable)
+                background.execute(wrapped)
             }
         }, timeout)
+    }
+}
+
+class StackTraceRunnable(val action: () -> Unit) : Runnable {
+    val stackTrace = Thread.currentThread().stackTrace.let {
+        it.takeLast(it.size - 3)
+    }.joinToString("\n")
+
+    override fun run() {
+        try {
+            action()
+        } catch (e: Exception) {
+            Log.e("PapyrusExecutor", "CAPTURED TRACE\n${stackTrace}")
+            throw e
+        }
     }
 }
